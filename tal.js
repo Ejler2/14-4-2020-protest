46b08034ad8ef79214e3057d6a0d716394807592
Noah <noahejler@gmail.com>
2020-04-14 13:41:03 +0200

    Update tal.js

--- tal.js
+++ tal.js
@@ -6,6 +6,16 @@ for (i = 0; i < tal1.length; i++) {
   for (j = 0; j < tal2.length; j++) {
     if(tal1[i] + tal2[j] == 8) { 
       gimig8.push([tal1[i], tal2[j]]);
+      tal1.splice(tal1[i]);
+      tal2.splice(tal2[j]);
+    } else if (tal1[i] + tal2[j] == 7) {
+        gimig8.push([tal1[i], tal2[j]]);
+        tal1.splice(tal1[i]);
+        tal2.splice(tal2[j]);
+    } else if (tal1[i] + tal2[j] == 9) {
+        gimig8.push([tal1[i], tal2[j]]);
+        tal1.splice(tal1[i]);
+        tal2.splice(tal2[j]);
     }
   }
 }
