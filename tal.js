e6429807437647934282f71ccdb04f25c8e5d156
Noah <noahejler@gmail.com>
2020-04-14 10:38:44 +0200

    Update tal.js

--- tal.js
+++ tal.js
@@ -1,12 +1,12 @@
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-let combined8 = [];
+let tal1 = [1, 2, 3];
+let tal2 = [4, 5, 6];
+let gimig8 = [];
 
-for (i = 0; i < arr1.length; i++) {
-  for (j = 0; j < arr2.length; j++) {
-    if(arr1[i] + arr2[j] == 8) { 
-      combined8.push([arr1[i], arr2[j]]);
+for (i = 0; i < tal1.length; i++) {
+  for (j = 0; j < tal2.length; j++) {
+    if(tal1[i] + tal2[j] == 8) { 
+      gimig8.push([tal1[i], tal2[j]]);
     }
   }
 }
-console.log(combined8);
+console.log(gimig8);
